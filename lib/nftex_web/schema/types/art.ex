d97e2fceb60a4b755e4218856535906aac7f71b8
Wagner Carvalho <wagner.carvalho@credisis.com.br>
2022-03-10 17:23:24 -0300

    adicao de documentacao

--- lib/nftex_web/schema/types/art.ex
+++ lib/nftex_web/schema/types/art.ex
@@ -2,17 +2,17 @@ defmodule NftexWeb.Schema.Types.Art do
   use Absinthe.Schema.Notation
 
   object :art do
-    field :id, non_null(:uuid4)
-    field :artist_name, non_null(:string)
-    field :description, non_null(:string)
-    field :price, non_null(:float)
-    field :hash, non_null(:string)
+    field :id, non_null(:uuid4), description: "Art ID"
+    field :artist_name, non_null(:string), description: "Artist name"
+    field :description, non_null(:string), description: "Art description"
+    field :price, non_null(:float), description: "Art price"
+    field :hash, non_null(:string), description: "Art hash"
   end
 
   input_object :create_art_input do
-    field :artist_name, non_null(:string)
-    field :description, non_null(:string)
-    field :price, non_null(:float)
-    field :hash, non_null(:string)
+    field :artist_name, non_null(:string), description: "Artist name"
+    field :description, non_null(:string), description: "Art description"
+    field :price, non_null(:float), description: "Art price"
+    field :hash, non_null(:string), description: "Art hash"
   end
 end
